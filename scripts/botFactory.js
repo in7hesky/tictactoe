6eeb708f4e6049290ae5b298fb501400e9751dca
in7hesky <anton.maiko.qa@gmail.com>
2023-03-16 23:22:27 +0300

    bot refactoring

--- scripts/botFactory.js
+++ scripts/botFactory.js
@@ -6,7 +6,6 @@ const botFactory = (name, symbol) => {
     const CENTER_CELL = 4
     const player = playerFactory(name, symbol)
 
-
     const takeTurn = (board) => {
         const freeCells = board.getFreeCells()
 
@@ -18,28 +17,30 @@ const botFactory = (name, symbol) => {
     const chooseMarkIndex = (board) => {
         const freeCells = board.getFreeCells()
 
-        for (let i = 0; i < freeCells.length; i++) {
-            let futureCells = [...board.getCells()]
-            futureCells[freeCells[i]] = symbol
-            if (arbiter.getVerdict(futureCells) > 0) return i
-        }
+        const botWinIndex = predictWinOutcome(board, freeCells)
+        if (botWinIndex > -1) return botWinIndex
 
-        const opponentSymbol = symbol === SYMBOLS[0] ? SYMBOLS[1] : SYMBOLS[0]
-        for (let i = 0; i < freeCells.length; i++) {
-            let futureCells = [...board.getCells()]
-            futureCells[freeCells[i]] = opponentSymbol
-            if (arbiter.getVerdict(futureCells) > 0) return i
-        }
+        const playerWinIndex = predictWinOutcome(board, freeCells, true)
+        if (playerWinIndex > -1) return playerWinIndex
 
         const centerIndex = freeCells.indexOf(CENTER_CELL)
-        
+
         return centerIndex > -1 ? centerIndex : getRandomNumber(0, freeCells.length)
     }
 
-    
+    const predictWinOutcome = (board, freeCells, forEnemy = false) => {
+        let subjectSymbol = !forEnemy ? symbol : (symbol === SYMBOLS[0] ? SYMBOLS[1] : SYMBOLS[0])
+        
+        for (let i = 0; i < freeCells.length; i++) {
+            let futureCells = [...board.getCells()]
+            futureCells[freeCells[i]] = subjectSymbol
+            if (arbiter.getVerdict(futureCells) > 0) return i
+        }
+
+        return -1
+    }
 
     const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min) + min)
-    
 
     return {
         ...player,
